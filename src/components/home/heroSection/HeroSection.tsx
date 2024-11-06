309385598219be507be8fe79c1f8d177baee78a8
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-11-06 19:43:34 +0600

    hero updated

--- src/components/home/heroSection/HeroSection.tsx
+++ src/components/home/heroSection/HeroSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import bannerimg from "@/assets/images/bannerbg.jpg";
-import Link from "next/link";
+// import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -26,13 +26,13 @@ const HeroSection = () => {
           effortlessly
         </p>
 
-        <Link href="news-feed">
+        {/* <Link href="news-feed">
           <button className="bg-emerald-400 text-white text-sm px-3 py-2 rounded transition-all duration-500 hover:bg-emerald-500 hover:pr-5">
             <span className="relative inline-block transition-all duration-500 after:content-['\00bb'] after:absolute after:opacity-0 after:top-0 after:right-[-10px] after:transition-all after:duration-500 hover:after:opacity-100 hover:after:right-[-10]">
               See Posts
             </span>
           </button>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
